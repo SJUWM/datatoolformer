f5d7aef89388dc1a446585fab388166a7073fe19
MADHUSHREE MURALIMOHAN NIJAGAL <madhushreemuralimoha@instgpu-02.cs.wisc.edu>
2023-12-02 20:51:53 -0600

    Adding the calc datasets math json

--- data_handling/to_hf_dataset.py
+++ data_handling/to_hf_dataset.py
@@ -18,16 +18,16 @@ if __name__ == '__main__':
         if len(sorted_keys) < 1:
             continue
         for i in range(len(sorted_keys)):
-            print("Inside for")
+            #print("Inside for")
             if sorted_keys[i][0] != 0:
                 output_text += tokenizer.decode(tokens[start:sorted_keys[i][0]])
                 start = sorted_keys[i][0]
             output_text += "<TOOLFORMER_API_START>" + sorted_keys[i][1] + "<TOOLFORMER_API_RESPONSE>" + str(sorted_keys[i][2]) + "<TOOLFORMER_API_END>"
-            print(output_text)
+           # print(output_text)
         if start < len(tokens)-1:
             output_text += tokenizer.decode(tokens[start:])
         hf_training_data["text"].append(output_text)
-        print(output_text)
+        #print(output_text)
     dataset = Dataset.from_dict(hf_training_data)
     dataset.push_to_hub("SJ1999/datatransformer1",token="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")
 
